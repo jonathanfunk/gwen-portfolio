8d81e1fd605aa0516b3bcba313ccb80952db8c73
Jonathan Funk <jonfunk88@hotmail.com>
2017-06-19 20:45:01 -0700

    theme name changed

---
+++ themes/gwen-portfolio/front-page.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * The main template file.
+ *
+ * @package Gwen_Portfolio_Theme
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+            <section class="header">
+            </section>
+            <section class="about">
+            </section>
+            <section class="projects">
+            </section>
+            <section class="resume">
+            </section>
+            <section class="contact">
+            </section>
+		</main><!-- #main -->
+	</div><!-- #primary -->
+<?php get_footer(); ?>
--- themes/gwen-portfolio/header.php
+++ themes/gwen-portfolio/header.php
@@ -2,7 +2,7 @@
 /**
  * The header for our theme.
  *
- * @package RED_Starter_Theme
+ * @package Gwen_Portfolio_Theme
  */
 
 ?><!DOCTYPE html>
