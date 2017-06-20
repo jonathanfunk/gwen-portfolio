4ba6e1c478b8e6918427da53da799ce7fca3f56c
Jonathan Funk <jonfunk88@hotmail.com>
2017-06-19 21:48:42 -0700

    contact form styled

--- themes/gwen-portfolio/front-page.php
+++ themes/gwen-portfolio/front-page.php
@@ -9,16 +9,20 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-            <section class="header">
-            </section>
-            <section class="about">
-            </section>
-            <section class="projects">
-            </section>
-            <section class="resume">
-            </section>
-            <section class="contact">
-            </section>
+      <section class="header">
+      </section>
+      <section class="about">
+      </section>
+      <section class="projects">
+      </section>
+      <section class="resume">
+      </section>
+      <section class="contact">
+        <div class="container">
+         <h2>Leave me a message</h2>
+         <?php echo do_shortcode( '[contact-form-7 id="6" title="Contact form 1"]' ); ?>
+        </div>
+      </section>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 <?php get_footer(); ?>
