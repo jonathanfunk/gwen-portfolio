b05870305b6c8ca75e471e896f68082056018e7b
Jonathan Funk <jonfunk88@hotmail.com>
2017-07-03 11:03:01 -0700

    contact group added

--- themes/gwen-portfolio/front-page.php
+++ themes/gwen-portfolio/front-page.php
@@ -194,7 +194,18 @@ get_header(); ?>
         <div class="container padding">
          <h2>Leave me a message</h2>
          <?php echo do_shortcode( '[contact-form-7 id="6" title="Contact form 1"]' ); ?>
+         <img 
+          class="contact-logo" 
+          src="<?php echo get_template_directory_uri() . '/assets/gwen-gong-white-logo.svg'; ?>" 
+          alt="Gwen Gong Logo" 
+        />
+         <h4>Thank</br>You</h4>
         </div>
+        <img 
+          class="contact-group" 
+          src="<?php echo get_template_directory_uri() . '/assets/contact-group.png'; ?>" 
+          alt="Contact Group" 
+        />
       </section>
 		</main><!-- #main -->
 	</div><!-- #primary -->
